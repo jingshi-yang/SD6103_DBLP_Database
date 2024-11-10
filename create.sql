9c4b054f9e7a6e347c390393240eb795f16c7850
Bocchi <jingshi.yang@outlook.com>
2024-11-10 19:19:28 +0800

    Update create.sql

--- create.sql
+++ create.sql
@@ -18,9 +18,9 @@ CREATE TABLE Publishers (
 -- Relation table betweeen authors and papers
 CREATE TABLE AuthorsPapers (
     AuthorID INT,
-    PaperID INT,
+    PublicationKey INT,
     AuthorOrder INT,
-    PRIMARY KEY (AuthorID, PaperID)
+    PRIMARY KEY (AuthorID, PublicationKey)
     -- To be added after read data
 );
 
@@ -36,104 +36,150 @@ CREATE TABLE Editors (
 -- Relation table between editors and papers
 CREATE TABLE PapersEditors (
     EditorID INT,
-    PaperID INT,
-    PRIMARY KEY (EditorID, PaperID),
+    PublicationKey INT,
+    PRIMARY KEY (EditorID, PublicationKey),
     FOREIGN KEY (EditorID) REFERENCES Editors(EditorID),
-    FOREIGN KEY (PaperID) REFERENCES Papers(PaperID)
 );
 
 -- Article
 CREATE TABLE Articles (
-    PublicationID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY,
     Title VARCHAR(1024) NOT NULL,
-    Key VARCHAR(50) NOT NULL,
     Year INT,
-    CDate VARCHAR(50),
+    Month VARCHAR(50),
     MDate VARCHAR(50),
     Volume VARCHAR(50),
     Number VARCHAR(50),
     Pages VARCHAR(50),
     DOI VARCHAR(100) UNIQUE,
-    ISBN VARCHAR(20) UNIQUE,
     URL VARCHAR(255),
     EE VARCHAR(255),
-    School VARCHAR(50),
-    CiteKey VARCHAR(100),
-    Address VARCHAR(255),
-    Series VARCHAR(50),
     Journal VARCHAR(50),
     CDROM VARCHAR(50),
     Cite VARCHAR(255),
     Publisher INT,
-    Chapter VARCHAR(50),
     Publnr VARCHAR(50),
-    Rel VARCHAR(50),
     Note VARCHAR(255),
     Stream VARCHAR(255),
 );
 
 -- InProceedings
 CREATE TABLE InProceedings (
-    InProceedingsID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY,
     Title VARCHAR(1024) NOT NULL,
-    Key VARCHAR(50) NOT NULL
-)
+    BookTitle VARCHAR(1024),
+    Year INT,
+    Number VARCHAR(50),
+    Pages VARCHAR(50),
+    DOI VARCHAR(100) UNIQUE,
+    URL VARCHAR(255),
+    EE VARCHAR(255),
+    CDROM VARCHAR(50),
+    Cite VARCHAR(255),
+    Publnr VARCHAR(50),
+    Note VARCHAR(255),
+    Stream VARCHAR(255),
+);
 
 -- Book
 CREATE TABLE Books (
-    BooksID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY,
     Title VARCHAR(1024) NOT NULL,
-    Key VARCHAR(50) NOT NULL
-)
+    BookTitle VARCHAR(1024),
+    Year INT,
+    Volume VARCHAR(50),
+    Pages VARCHAR(50),
+    DOI VARCHAR(100) UNIQUE,
+    ISBN VARCHAR(20) UNIQUE,
+    URL VARCHAR(255),
+    EE VARCHAR(255),
+    School VARCHAR(50),
+    Series VARCHAR(50),
+    Cite VARCHAR(255),
+    Publisher INT,
+    Note VARCHAR(255),
+);
 
 -- InCollection
 CREATE TABLE InCollections (
-    InCollectionsID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY,
     Title VARCHAR(1024) NOT NULL,
-    Key VARCHAR(50) NOT NULL
-)
+    BookTitle VARCHAR(1024),
+    Year INT,
+    Pages VARCHAR(50),
+    DOI VARCHAR(100) UNIQUE,
+    URL VARCHAR(255),
+    EE VARCHAR(255),
+    CDROM VARCHAR(50),
+    Cite VARCHAR(255),
+    Publisher INT,
+    Note VARCHAR(255),
+);
 
 -- PhDThesis
 CREATE TABLE PhDThesis (
-    PhDThesisID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY,
     Title VARCHAR(1024) NOT NULL,
-    Key VARCHAR(50) NOT NULL
-)
+    Year INT,
+    Volume VARCHAR(50),
+    Pages VARCHAR(50),
+    DOI VARCHAR(100) UNIQUE,
+    ISBN VARCHAR(20) UNIQUE,
+    EE VARCHAR(255),
+    School VARCHAR(50),
+    Address VARCHAR(255),
+    Series VARCHAR(50),
+    Publisher INT,
+);
 
 -- MasterThesis
 CREATE TABLE MasterThesis (
-    MasterThesisID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY,
     Title VARCHAR(1024) NOT NULL,
-    Key VARCHAR(50) NOT NULL
-)
+    Year INT,
+    Pages VARCHAR(50),
+    DOI VARCHAR(100) UNIQUE,
+    ISBN VARCHAR(20) UNIQUE,
+    EE VARCHAR(255),
+    School VARCHAR(50),
+    Address VARCHAR(255),
+    Publisher INT,
+    Note VARCHAR(255),
+);
 
 -- www
 CREATE TABLE www (
-    wwwID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY,
     Title VARCHAR(1024) NOT NULL,
-    Key VARCHAR(50) NOT NULL
-)
+    Year INT,
+    DOI VARCHAR(100) UNIQUE,
+    ISBN VARCHAR(20) UNIQUE,
+    URL VARCHAR(255),
+    Cite VARCHAR(255),
+    Note VARCHAR(255),
+);
 
 -- Data
 CREATE TABLE Data (
-    DataID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY,
     Title VARCHAR(1024) NOT NULL,
-    Key VARCHAR(50) NOT NULL
-)
-
--- CrossRef
-CREATE TABLE CrossRefs (
-    ProceedingsID INT,
-    PublicationsID INT,
-    FOREIGN KEY (ProceedingsID),
-    FOREIGN KEY (PublicationID)
+    Year INT,
+    Number VARCHAR(50),
+    DOI VARCHAR(100) UNIQUE,
+    EE VARCHAR(255),
+    School VARCHAR(50),
+    Series VARCHAR(50),
+    Publisher INT,
+    Rel VARCHAR(50),
+    Stream VARCHAR(255),
 );
 
 -- Proceedings
 CREATE TABLE Proceedings (
-    ProceedingsID INT PRIMARY KEY AUTO_INCREMENT,
+    PublicationKey VARCHAR(50) PRIMARY KEY AUTO_INCREMENT,
     Key VARCHAR(255) NOT NULL UNIQUE,
-    Name VARCHAR(255) NOT NULL,
+    Title VARCHAR(255) NOT NULL,
+    BookTitkle VARCHAR(1024),
     Year INT NOT NULL,
     PublisherID INT,
     Location VARCHAR(255),
@@ -141,3 +187,10 @@ CREATE TABLE Proceedings (
 );
 
 
+-- CrossRef
+CREATE TABLE CrossRefs (
+    PublicationKey INT,
+    ProceedingsKEY INT,
+    FOREIGN KEY (PublicationKey),
+    FOREIGN KEY (ProceedingsKey)
+);
